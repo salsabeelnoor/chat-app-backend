1cf0a2558e68f45fa85937ef85a3a7de93d38355
salsabeelnoor <salsabeelnoor@gmail.com>
2025-01-12 19:19:04 +0600

    auth and message api ready

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 // const express = require('express');
 import express from 'express';
 import  authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+
 import dotenv from 'dotenv';
 import { connectDB } from './lib/db.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
@@ -10,7 +13,10 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(express.json()); //alows to extract the json data outof the request body
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log("server is running on port PORT: ", PORT);
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { login, logout, signup } from '../controllers/auth.controller.js';
+import { login, logout, signup, updateProfile, checkAuth } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
@@ -8,5 +9,9 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
